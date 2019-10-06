84e4cb7535f4e4082283d35964bf4c45c329b3db
Nin Alamo <nino.alamo@kmc.solutions>
2019-10-06 11:24:22 +0800

    Done with application layer

---
+++ clear-arch/application/BaseAuditor.cs
@@ -0,0 +1,34 @@
+﻿using application.interfaces;
+using System;
+
+namespace application
+{
+    public abstract class BaseAuditor : ITakeCredit
+    {
+        public string CreatedBy { get; set; }
+        public string ModifiedBy { get; set; }
+
+        //public static void Create(IErpDbContext _context, string creator, object before, object after, string table, long? id = null)
+        //{
+        //    try
+        //    {
+        //        _context.AuditTrails.Add(new AuditTrail
+        //        {
+        //            BeforeCommit = Auditify.ToJsonString(before),
+        //            AfterCommit = Auditify.ToJsonString(after),
+        //            CreatedBy = creator,
+        //            ModifiedBy = creator,
+        //            ObjectID = id,
+        //            Table = table
+        //        });
+
+        //        _context.SaveChangesAsync(CancellationToken.None).ConfigureAwait(false);
+        //    }
+        //    catch
+        //    {
+
+        //    }
+
+        //}
+    }
+}
---
+++ clear-arch/application/interfaces/ITakeCredit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace application.interfaces
+{
+    public interface ITakeCredit
+    {
+        string CreatedBy { get; set; }
+        string ModifiedBy { get; set; }
+    }
+}
