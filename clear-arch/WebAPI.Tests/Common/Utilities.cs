cf2ed8f2dbbf17dfa8feabe79ab58dccff58e916
Nin Alamo <nino.alamo@kmc.solutions>
2020-06-01 16:32:14 +0800

    Test

---
+++ clear-arch/WebAPI.Tests/Common/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Persistence;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.Tests.Common
+{
+    public class Utilities
+    {
+        public static StringContent GetRequestContent(object obj)
+        {
+            return new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json");
+        }
+
+        public static async Task<T> GetResponseContent<T>(HttpResponseMessage response)
+        {
+            var stringResponse = await response.Content.ReadAsStringAsync();
+
+            var result = JsonConvert.DeserializeObject<T>(stringResponse);
+
+            return result;
+        }
+
+        public static void InitializeDbForTests(ApplicationDbContext context)
+        {
+            
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ clear-arch/WebAPI.Tests/Controllers/People/Create.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using WebAPI.Tests.Common;
+using Xunit;
+
+namespace WebAPI.Tests.Controllers.People
+{
+    public class Create : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly CustomWebApplicationFactory<Startup> factory;
+
+        public Create(CustomWebApplicationFactory<Startup> factoryArg)
+        {
+            factory = factoryArg;
+        }
+
+        [Fact]
+        public async Task AddsANewPersonSuccessfully()
+        {
+            var client = factory.GetAnonymousClient();
+
+            var response = await client.PostAsync("", null);
+        }
+    }
+}
